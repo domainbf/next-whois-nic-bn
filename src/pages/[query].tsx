a5154d6b6d4e5360a3057df6d56fc18104cea4db
不讲李 <158622671+domainbf@users.noreply.github.com>
2024-08-07 18:37:14 +0800

    Update [query].tsx

--- src/pages/[query].tsx
+++ src/pages/[query].tsx
@@ -379,7 +379,7 @@ const ResultComp = React.forwardRef<HTMLDivElement, Props>(
             <CardTitle
               className={`flex flex-row items-center text-lg md:text-xl`}
             >
-              详情如下:
+              {result?.status === "registered" ? "已注册" : "未注册"}:
               {!isCapture && (
                 <Drawer>
                   <DrawerTrigger asChild>
