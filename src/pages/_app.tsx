b3f75671364c1bcb24b93912ebad8c24e62689a6
不讲李 <158622671+domainbf@users.noreply.github.com>
2024-08-07 13:32:21 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,8 +27,8 @@ const announcements = [
   "我们不存储个记录您的所有查询内容",
   "如有问题请邮箱联系：a@f.af",
   "我们提供域名注册和过期域名抢注服务",
-  "域名注册及延期交付订单：NIC.BN",
-  "立即可购买的域名列表：DOMAIN.BF",
+  <Link href="https://www.nic.bn" target="_blank" key="1">域名注册及延期交付订单：NIC.BN</Link>,
+  <Link href="https://www.domain.bf" target="_blank" key="2">立即可购买的域名列表：DOMAIN.BF</Link>,
 ];
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -50,6 +50,46 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Head>
+        <title>{siteTitle}</title>
+        <meta name="description" content={siteDescription} />
+        <meta name="keywords" content={siteKeywords} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Toaster />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <div className={cn(`relative w-full h-full`, inter.className)}> {/* 使用 inter.className */}
+          <div
+            className={cn(
+              `flex flex-row items-center space-x-4`,
+              `absolute top-4 left-4 z-50`
+            )}
+          >
+            <img src="/gg.gif" alt="Logo" className="w-8 h-8" />
+            <div className="text-sm">{currentAnnouncement}</div>
+          </div>
+          <div
+            className={cn(
+              `absolute top-4 right-4 flex flex-row items-center z-50 space-x-2`
+            )}
+          >
+            <ThemeToggle />
+            <Link href={`https://www.domain.bf`} target={`_blank`}>
+              <Button variant={`outline`} size={`icon`} tapEnabled>
+                <svg
+                  role="img"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className={`w-5 h-5 fill-primary`}
+                >
+                  <title>Earth</title>
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm1-13h-2v6h2V7zm0 8h-2v2h2v-2
+<>
       <Head>
         <title>{siteTitle}</title>
         <meta name="description" content={siteDescription} />
