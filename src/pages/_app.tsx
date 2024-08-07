471b992697c107b44074b700cd716c5bf7263824
不讲李 <158622671+domainbf@users.noreply.github.com>
2024-08-07 12:59:08 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
@@ -8,25 +9,48 @@ import Link from "next/link";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ThemeToggle } from "@/components/theme-switch";
 import { strEnv } from "@/lib/env";
-import { inter } from "@/lib/fonts";
 
-const siteTitle = strEnv("NEXT_PUBLIC_SITE_TITLE", "Next Whois UI");
+
+const siteTitle = strEnv("NEXT_PUBLIC_SITE_TITLE", "Whois");
 const siteDescription = strEnv(
   "NEXT_PUBLIC_SITE_DESCRIPTION",
-  "Next Whois Lookup Tool, Support Domain/IPv4/IPv6/ASN/CIDR Whois Lookup, Provided Beautiful, Clean and Simple UI.",
+  "Whois Lookup Tool, Support Domain/IPv4/IPv6/ASN/CIDR Whois Lookup, Provided Beautiful, Clean and Simple UI."
 );
 const siteKeywords = strEnv(
   "NEXT_PUBLIC_SITE_KEYWORDS",
-  "Whois, Lookup, Tool, Next Whois UI",
+  "Whois, Lookup, Tool, whois"
 );
 
+const announcements = [
+  "我们不存储个记录您的所有查询内容",
+  "如有问题请邮箱联系：a@f.af",
+  "我们提供域名注册和过期域名抢注服务",
+  "域名注册及延期交付订单：NIC.BN",
+  "立即可购买的域名列表：domain.bf",
+];
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [announcementIndex, setAnnouncementIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setAnnouncementIndex((prevIndex) =>
+        prevIndex === announcements.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const currentAnnouncement = announcements[announcementIndex];
+
   return (
     <>
       <Head>
         <title>{siteTitle}</title>
         <meta name="description" content={siteDescription} />
-        <meta name="tags" content={siteKeywords} />
         <meta name="keywords" content={siteKeywords} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
@@ -40,14 +64,20 @@ export default function App({ Component, pageProps }: AppProps) {
         <div className={cn(`relative w-full h-full`, inter.className)}>
           <div
             className={cn(
-              `absolute top-4 right-4 flex flex-row items-center z-50 space-x-2`,
+              `flex flex-row items-center space-x-4`,
+              `absolute top-4 left-4 z-50`
+            )}
+          >
+            <img src="/gg.gif" alt="Logo" className="w-8 h-8" />
+            <div className="text-sm">{currentAnnouncement}</div>
+          </div>
+          <div
+            className={cn(
+              `absolute top-4 right-4 flex flex-row items-center z-50 space-x-2`
             )}
           >
             <ThemeToggle />
-            <Link
-              href={`https://github.com/zmh-program/next-whois-ui`}
-              target={`_blank`}
-            >
+            <Link href={`https://www.domain.bf`} target={`_blank`}>
               <Button variant={`outline`} size={`icon`} tapEnabled>
                 <svg
                   role="img"
@@ -55,8 +85,9 @@ export default function App({ Component, pageProps }: AppProps) {
                   xmlns="http://www.w3.org/2000/svg"
                   className={`w-5 h-5 fill-primary`}
                 >
-                  <title>GitHub</title>
-                  <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                  <title>Earth</title>
+                  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm1-13h-2v6h2V7zm0 8h-2v2h2v-2z" />
+                  <path d="M12 4c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3zm0 10c-2.67 0-5-2.33-5-5s2.33-5 5-5 5 2.33 5 5-2.33 5-5 5z" />
                 </svg>
               </Button>
             </Link>
