5097e3e325a606327a64b87a14cd180654137c0b
不讲李 <158622671+domainbf@users.noreply.github.com>
2024-08-07 12:48:20 +0800

    Delete src/lib/_app.tsx

--- src/lib/_app.tsx
+++
@@ -1,8 +0,0 @@
-// src/pages/_app.tsx
-import { strEnv } from '@/utils'; // 确保路径正确
-import { ThemeToggle } from '@/components/theme-switch';
-
-const siteTitle = strEnv("NEXT_PUBLIC_SITE_TITLE", "Whois");
-const siteDescription = strEnv("NEXT_PUBLIC_SITE_DESCRIPTION", "Whois Lookup Tool, Support Domain/IPv4/IPv6/ASN/CIDR Whois Lookup, Provided Beautiful, Clean and Simple UI.");
-
-// 其他代码...
