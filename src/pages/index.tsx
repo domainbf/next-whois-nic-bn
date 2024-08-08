5138e09bf75e8f0d2f61fdd1f56588efea8863a8
不讲李 <158622671+domainbf@users.noreply.github.com>
2024-08-09 01:52:35 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,146 +1,209 @@
-import React from 'react';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
   CheckIcon,
+  ChevronRight,
   CornerDownRight,
+  Link2,
   Loader2,
   Search,
   Send,
+  Trash2,
+  Undo2,
 } from "lucide-react";
-import Head from "next/head";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import { cn, isEnter, toSearchURI } from "@/lib/utils";
-import { VERSION } from "@/lib/env";
+import { addHistory, listHistory, removeHistory } from "@/lib/history";
+import Icon from "@/components/icon";
+import Clickable from "@/components/motion/clickable";
 
-interface FixedInputProps {
-  inputDomain: string;
-  setInputDomain: (value: string) => void;
-  loading: boolean;
-  setLoading: (value: boolean) => void;
-}
+export default function Home() {
+  const [domain, setDomain] = React.useState<string>("");
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [history, setHistory] = React.useState<string[]>([]);
+  const [trashMode, setTrashMode] = React.useState<boolean>(false);
 
-const FixedInput: React.FC<FixedInputProps> = ({ inputDomain, setInputDomain, loading, setLoading }) => {
+  useEffect(() => {
+    setHistory(listHistory());
+  }, []);
+
+  useEffect(() => {
+    const pixelScript = document.createElement("script");
+    pixelScript.defer = true;
+    pixelScript.src = "https://china.tn/pixel/vyneXbR4gSRGqFfs";
+    document.body.appendChild(pixelScript);
+
+    return () => {
+      document.body.removeChild(pixelScript);
+    };
+  }, []);
+
   const goStage = (target: string) => {
     setLoading(true);
-    window.history.pushState({}, '', toSearchURI(inputDomain));
   };
 
   return (
-    <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-        />
-        <meta property="og:title" content="WHOIS.LS - 查询域名和IP信息" />
-        <meta property="og:description" content="使用 WHOIS.LS 查询域名、IPv4、IPv6、ASN 和 CIDR 信息。" />
-        <meta property="og:image" content="https://whois.ls/whoislogo.jpeg" />
-        <meta property="og:url" content="https://whois.ls" />
-        <meta property="og:type" content="website" />
-      </Head>
-      <main className={"w-full h-full grid place-items-center p-4 md:p-6"}>
-        <div className={"flex flex-col items-center w-full h-fit max-w-[568px]"}>
-          <h1
+    <main className={"w-full h-full grid place-items-center p-4 md:p-6"}>
+      <div className={"flex flex-col items-center w-full h-fit max-w-[568px]"}>
+        <h1
+          className={
+            "text-lg md:text-2xl lg:text-3xl font-bold flex flex-row items-center select-none"
+          }
+        >
+          <Search className={`w-4 h-4 md:w-6 md:h-6 mr-1 md:mr-1.5 shrink-0`} />
+          WHOIS.LS
+        </h1>
+        <div
+          className={"flex flex-row items-center flex-wrap justify-center mt-1"}
+        >
+          <div
             className={
-              "text-lg md:text-2xl lg:text-3xl font-bold flex flex-row items-center select-none"
+              "flex mx-1 my-0.5 flex-row items-center text-md text-secondary transition hover:text-primary cursor-pointer"
             }
           >
-            <Search
-              className={`w-4 h-4 md:w-6 md:h-6 mr-1 md:mr-1.5 shrink-0`}
-            />
-            WHOIS.LS
-          </h1>
-          <div
-            className={"flex flex-row items-center flex-wrap justify-center mt-1"}
-          >
-            <div
-              className={
-                "flex mx-1 my-0.5 flex-row items-center text-md text-secondary transition hover:text-primary cursor-pointer"
-              }
-            >
-              <CheckIcon className={`w-4 h-4 mr-1 shrink-0`} />
-              <p>Domain</p>
-            </div>
-            <div
-              className={
-                "flex mx-1 my-0.5 flex-row items-center text-md text-secondary transition hover:text-primary cursor-pointer"
-              }
-            >
-              <CheckIcon className={`w-4 h-4 mr-1 shrink-0`} />
-              <p>IPv4</p>
-            </div>
-            <div
-              className={
-                "flex mx-1 my-0.5 flex-row items-center text-md text-secondary transition hover:text-primary cursor-pointer"
-              }
-            >
-              <CheckIcon className={`w-4 h-4 mr-1 shrink-0`} />
-              <p>IPv6</p>
-            </div>
-            <div
-              className={
-                "flex mx-1 my-0.5 flex-row items-center text-md text-secondary transition hover:text-primary cursor-pointer"
-              }
-            >
-              <CheckIcon className={`w-4 h-4 mr-1 shrink-0`} />
-              <p>ASN</p>
-            </div>
-            <div
-              className={
-                "flex mx-1 my-0.5 flex-row items-center text-md text-secondary transition hover:text-primary cursor-pointer"
-              }
-            >
-              <CheckIcon className={`w-4 h-4 mr-1 shrink-0`} />
-              <p>CIDR</p>
-            </div>
-          </div>
-          <div className={"relative flex flex-row items-center w-full mt-2"}>
-            <Input
-              className={`w-full text-center transition-all duration-300 hover:shadow`}
-              placeholder={`输入要查询的内容 ( whois.ls, 8.8.8.8)`}
-              value={inputDomain}
-              onChange={(e) => setInputDomain(e.target.value)}
-              onKeyDown={(e) => {
-                if (isEnter(e)) {
-                  goStage(inputDomain);
-                }
-              }}
-              style={{ touchAction: 'manipulation', userSelect: 'none' }} // 禁止页面缩放
-            />
-            <Link
-              href={toSearchURI(inputDomain)}
-              className={`absolute right-0`}
-              onClick={() => inputDomain.length > 0 && goStage(inputDomain)}
-            >
-              <Button
-                size={`icon`}
-                variant={`outline`}
-                className={`rounded-l-none transition`}
-                disabled={inputDomain.length === 0}
-              >
-                {loading ? (
-                  <Loader2 className={`w-4 h-4 animate-spin`} />
-                ) : (
-                  <Send className={`w-4 h-4`} />
-                )}
-              </Button>
-            </Link>
+            <CheckIcon className={`w-4 h-4 mr-1 shrink-0`} />
+            <p>Domain</p>
           </div>
           <div
-            className={cn(
-              `flex items-center flex-row w-full text-xs mt-1.5 select-none text-secondary transition`,
-              loading && "text-primary",
-            )}
+            className={
+              "flex mx-1 my-0.5 flex-row items-center text-md text-secondary transition hover:text-primary cursor-pointer"
+            }
           >
-            <div className={`flex-grow`} />
-            <CornerDownRight className={`w-3 h-3 mr-1`} />
-            <p className={`px-1 py-0.5 border rounded-md`}>Enter</p>
+            <CheckIcon className={`w-4 h-4 mr-1 shrink-0`} />
+            <p>IPv4</p>
+          </div>
+          <div
+            className={
+              "flex mx-1 my-0.5 flex-row items-center text-md text-secondary transition hover:text-primary cursor-pointer"
+            }
+          >
+            <CheckIcon className={`w-4 h-4 mr-1 shrink-0`} />
+            <p>IPv6</p>
+          </div>
+          <div
+            className={
+              "flex mx-1 my-0.5 flex-row items-center text-md text-secondary transition hover:text-primary cursor-pointer"
+            }
+          >
+            <CheckIcon className={`w-4 h-4 mr-1 shrink-0`} />
+            <p>ASN</p>
+          </div>
+          <div
+            className={
+              "flex mx-1 my-0.5 flex-row items-center text-md text-secondary transition hover:text-primary cursor-pointer"
+            }
+          >
+            <CheckIcon className={`w-4 h-4 mr-1 shrink-0`} />
+            <p>CIDR</p>
           </div>
         </div>
-      </main>
-    </>
-  );
-};
+        <div className={"relative flex flex-row items-center w-full mt-2"}>
+          <Input
+            className={`w-full text-center transition-all duration-300 hover:shadow`}
+            placeholder={`domain name (e.g. google.com, 8.8.8.8)`}
+            value={domain}
+            onChange={(e) => setDomain(e.target.value)}
+            onKeyDown={(e) => {
+              if (isEnter(e)) {
+                goStage(domain);
+                window.location.href = toSearchURI(domain);
+              }
+            }}
+            style={{ fontSize: '16px' }} // 确保在移动端输入时不会缩放
+          />
+          <Link
+            href={toSearchURI(domain)}
+            className={`absolute right-0`}
+            onClick={() => domain.length > 0 && goStage(domain)}
+          >
+            <Button
+              size={`icon`}
+              variant={`outline`}
+              className={`rounded-l-none transition`}
+              disabled={domain.length === 0}
+            >
+              {loading ? (
+                <Loader2 className={`w-4 h-4 animate-spin`} />
+              ) : (
+                <Send className={`w-4 h-4`} />
+              )}
+            </Button>
+          </Link>
+        </div>
+        <div
+          className={cn(
+            `flex items-center flex-row w-full text-xs mt-1.5 select-none text-secondary transition`,
+            loading && "text-primary",
+          )}
+        >
+          <div className={`flex-grow`} />
+          <CornerDownRight className={`w-3 h-3 mr-1`} />
+          <p className={`px-1 py-0.5 border rounded-md`}>Enter</p>
+        </div>
+        {history.length > 0 && (
+          <>
+            <div
+              className={`mt-6 md:mt-8 grid grid-cols-1 md:grid-cols-2 gap-2 w-full h-fit`}
+            >
+              {history.map((item, index) => (
+                <Clickable tapScale={0.985} key={index}>
+                  <Link
+                    className={cn(
+                      `text-sm w-full h-full`,
+                      `flex flex-row items-center`,
+                      `border rounded-md pl-3.5 pr-2.5 py-2 text-secondary`,
+                      `group transition duration-300 bg-muted/20 hover:bg-muted/40`,
+                      `hover:text-primary hover:border-hover`,
+                    )}
+                    href={toSearchURI(item)}
+                    onClick={(e) => {
+                      if (trashMode) {
+                        e.preventDefault();
 
-export default FixedInput;
+                        removeHistory(item);
+                        setHistory(listHistory());
+                        return;
+                      } else {
+                        goStage(item);
+                      }
+                    }}
+                  >
+                    <Icon
+                      icon={!trashMode ? <Link2 /> : <Trash2 />}
+                      className={cn(
+                        "w-4 h-4 mr-1 shrink-0",
+                        trashMode && "text-red-600/80",
+                      )}
+                    />
+                    <p
+                      className={`grow text-ellipsis overflow-hidden text-primary`}
+                    >
+                      {item}
+                    </p>
+                    <ChevronRight
+                      className={`transition-all shrink-0 w-4 h-4 ml-auto mr-0.5`}
+                    />
+                  </Link>
+                </Clickable>
+              ))}
+            </div>
+            <div className={`flex flex-row items-center w-full mt-2`}>
+              <Button
+                variant={`outline`}
+                size={`icon-sm`}
+                onClick={() => setTrashMode(!trashMode)}
+                tapClassName={`ml-auto`}
+              >
+                <Icon
+                  icon={trashMode ? <Undo2 /> : <Trash2 />}
+                  className={`w-3.5 h-3.5`}
+                />
+              </Button>
+            </div>
+          </>
+        )}
+      </div>
+    </main>
+  );
+}
